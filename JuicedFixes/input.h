ab4116c72cf4b57751d14590c5614b24660de6d3
Grim Maple <grimmaple95@gmail.com>
2019-01-05 16:58:25 +0300

    Some stuff figured out

---
+++ JuicedFixes/input.h
@@ -0,0 +1,76 @@
+#pragma once
+
+
+enum Axles
+{
+	Steering = 0
+};
+
+enum MenuButtons
+{
+	/* Missing stuff:
+	3 -- has no idea what it does
+	9 -- shows drivers names?
+	13 -- IDK
+
+	*/
+	Left = 0,//	ALSO
+	LeftDigital = 1,
+	Right = 2, // ALSO CHANGE VIEW
+	RightDigital = 3,
+	Up = 4, // ALSO HORN
+	UpDigital = 5,
+	Down = 6, // ALSO GEAR DOWN
+	DownDigital = 7,
+	Accept = 8, //	--ACCEPT
+	Menu9 = 9,
+	PageDown = 10, //	--PGDN(option)
+	Tab = 11, //	--TAB(option)
+	Back = 12, //	--BACK
+	Back2 = 13,
+	Menu14 = 14,
+	Menu15 = 15
+	//Pause = 1,
+	//LookBack = 5,
+	//GearUp = 7,
+};
+
+enum RaceButtons
+{
+	Race0 = 0,
+	Pause = 1,
+	ChangeView = 2,
+	Nitro = 3,
+	Horn = 4,
+	LookBack = 5,
+	GearDown = 6,
+	GearUp = 7,
+	Skip = 8,
+	Race9 = 9,
+	Race10 = 10,
+	Race11 = 11,
+	Race12 = 12,
+	Race13 = 13,
+	Race14 = 14,
+	Race15 = 15
+};
+
+enum ControlType
+{
+	Menu = 1,
+	Race = 0
+};
+
+class IInput
+{
+public:
+	void SetControlType(ControlType type)
+	{
+		controlType = type;
+	}
+protected:
+	bool SinglePress(int keyCode);
+
+private:
+	ControlType controlType;
+};
